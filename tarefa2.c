5c1c4a0e05314552447eaf99909a6369942f948f
Raiane Roza <raianeroza@id.uff.br>
2017-11-23 15:46:47 -0200

    Nada

--- tarefa2.c
+++ tarefa2.c
@@ -11,16 +11,17 @@ typedef double (*sistfunc)();
 
 double f(double y[], double t)
 {
-	return y[2];
-}
-double g(double y[], double t)
-{
 	return y[1];
 }
-double h(double y[], double t)
+double f1(double y[], double t)
+{
+	return y[2];
+}
+double f2(double y[], double t)
 {
 	return (2.*y[2]) - y[1] - exp(t);
 }
+
 
 double grunge(sistfunc func[], double y[], double t, double h)
 {
@@ -50,28 +51,25 @@ double grunge(sistfunc func[], double y[], double t, double h)
 
 main(int argc, char **argv)
 {
-	double t0=0., y0=1., yl=2., yll=0., h=(3. - 0.)/N, tn, yt[n], y;
+	double t0=0., y1[n]={1., 2., 0.}, h=(3. - 0.)/N, tn, yt[N], y2;
+	int i;
 	
-	sistfunc equacoes[n]={f,g,h};
-	//sistfunc equacoes1[n]={f,g,h};
+	sistfunc equacoes1[n]={f,f1,f2};
 	
-	op=fopen(argv[1], "w");
+	//op=fopen(argv[1], "w");
 	
 	tn=t0;
-	yt[0]=y0;
-	yt[1]=yl;
-	yt[2]=yll;
-	
-	y=y0;
-	
+	yt[0]=y1[0];
+
+	i=1;
 	while(tn<3.){
 		
-		fprintf(op,"%.2lf\t%.2lf\n", tn, y);
+		yt[i] = grunge(equacoes1,y1,tn,h);
+		printf("%lf\t%lf\n", tn, yt[i]);
 		
-		y = grunge(equacoes,yt,tn,h);
-		
-		tn=tn+h;	
+		tn+=h;	
+		i++;
 	}
 	
-	fclose(op);
+	//fclose(op);
 }
